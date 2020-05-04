165fab6fb4cf4bc576612a20b09065f6542daeba
Amin Ahrabian <aahrabian@gmail.com>
2020-05-04 11:03:59 +0200

    Define the type of the message which is sent from user

--- bot_request_manager.py
+++ bot_request_manager.py
@@ -1,5 +1,9 @@
 from utils import Utils
 
+TYPE_PHOTO = 0
+TYPE_TEXT = 1
+TYPE_WELCOME = 2
+
 class BotRequestManager:
 
   def __init__(self, request):
@@ -16,9 +20,24 @@ class BotRequestManager:
 
     Utils.print_json(self.request)
 
-    
+    request = self.request
 
-    pass
+    keys = request["message"].keys()
+
+    message = request['message']
+    
+    if ("caption" in keys or "photo" in keys):
+      
+      self.type = TYPE_PHOTO
+      
+    elif ("text" in keys):
+            
+      self.type = TYPE_TEXT
+
+      # If it was the user's first message
+      if message['text'] == "/start":
+
+        self.type = TYPE_WELCOME              
 
   """ Does different task based on Type """
   def get_result(self):
